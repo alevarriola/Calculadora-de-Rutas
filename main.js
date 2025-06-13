9d06464a62dc3e5b544c2e4c4e2db22175c163a0
Alejandro Arriola <alev_arriola@hotmail.com>
2025-06-13 13:10:54 -0300

    Agrege pasos para cambiar funcion del boton, agrege input de fila y columna, las celdas pasan a ser botones

--- main.js
+++ main.js
@@ -1,17 +1,67 @@
 
-const generador = document.getElementById("generar");
+const boton = document.getElementById("boton-principal");
 const contenedor = document.getElementById("tablero");
+const cuerpo = document.getElementById("cuerpo")
+
+let paso = 0;
 
 
-generador.addEventListener("click", function() {
+boton.addEventListener("click", function() {
+
+    if (paso == 0) {
+
+        // Crear inputs
+        const inputFilas = document.createElement("input");
+        inputFilas.type = "number";
+        inputFilas.id = "input-filas";
+        inputFilas.placeholder = "Filas";
+        inputFilas.className = "flex border border-blue-300 bg-blue-100 text-blue-900 rounded-xl p-2 m-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400";
+
+        const inputColumnas = document.createElement("input");
+        inputColumnas.type = "number";
+        inputColumnas.id = "input-columnas";
+        inputColumnas.placeholder = "Columnas";
+        inputColumnas.className = "flex border border-blue-300 bg-blue-100 text-blue-900 rounded-xl p-2 m-2 shadow-sm focus:outline-none focus:ring-2 focus:ring-blue-400";
+
+        cuerpo.appendChild(inputFilas);
+        cuerpo.appendChild(inputColumnas);
+
+        boton.textContent = "Generar tablero";
+        paso = 1;
+    }
+
+    else if (paso === 1) {
+
+        // generamos el tablero
+        const filas = parseInt(document.getElementById("input-filas").value);
+        const columnas = parseInt(document.getElementById("input-columnas").value);
+
+        generarTablero(filas, columnas);
+
+        document.getElementById("input-filas").remove();
+        document.getElementById("input-columnas").remove();
+
+        boton.textContent = "Generar caminos";
+        paso = 2;
+    }
     
-    // limpiar el tablero anterior, eliminar luego cuando avances mas, hace que el boton haga off
-    contenedor.innerHTML = "";
+    else if (paso === 2) {
+
+        // generamos los caminos
+        generarCaminos();
+        boton.textContent = "Resetear tablero"; 
+        paso = 3;
+    }
+
+     else if (paso === 3) {
+        resetearTablero();
+        boton.textContent = "Iniciar";
+        paso = 0;
+     }
+});
 
 
-    // definimos filas y columnas
-    const filas = 30;
-    const columnas = 30;
+function generarTablero(filas, columnas) {
 
     // definimos nuestro contenedor para el tablero
     contenedor.style.display = "grid";
@@ -28,10 +78,15 @@ generador.addEventListener("click", function() {
         for (let j = 0; j < columnas; j++) {
 
             // Crear elemento celda
-            const celda = document.createElement("div");
+            const celda = document.createElement("button");
             celda.className = "bg-blue-300 border border-white";
             celda.style.width = "20px";
             celda.style.height = "20px";
+
+            // cada celda tiene esta funcion adentro
+            celda.addEventListener("click", function() {
+                celda.classList.toggle("bg-red-300");
+            });
 
             // Agregarlo al body
             contenedor.appendChild(celda);
@@ -39,4 +94,14 @@ generador.addEventListener("click", function() {
         }
         tablero.push(fila);
     }
-});
+};
+
+function generarCaminos() {
+    
+};
+
+function resetearTablero() {
+    document.getElementById("tablero").innerHTML = "";
+    contenedor.innerHTML = "";
+    contenedor.style.height = "0";
+};
